2b00299a0c76baafb86c65c3c4f375d3e1259ad4
adamcumiskey <adam.cumiskey@gmail.com>
2018-03-14 11:09:25 -0400

    Create a binary tree class and dynamically insert random values

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ const nodeWidth = 15 * scale
 const horizontalSpace = 50 * scale
 const verticalSpace = 50 * scale
 
-class Tree {
-  constructor(data, children) {
-    this.data = data
-    this.children = children
+const compare = function(a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    if (a === b) { return 0 }
+    if (a < b) { return -1 }
+    if (a > b) { return 1 }
+  } else if (typeof a === 'string' && typeof b === 'string') {
+    return a.localeCompare(b)
+  } else if (a.compare !== undefined && typeof a === typeof b) {
+    return a.compare(b)
   }
+}
 
+class Tree {
   get width() {
     if (this.children === undefined || this.children.length == 0) {
       return nodeWidth
@@ -23,22 +30,58 @@ class Tree {
 
 }
 
-const tree = new Tree(
-  5,
-  [new Tree(6, [new Tree(10), new Tree(5), new Tree(1)]),
-  new Tree(8, [new Tree(10), new Tree(1)]),
-  new Tree(10, [new Tree(10), new Tree(15), new Tree(18, [new Tree(5)])])]
-)
+class BinaryTree extends Tree {
+  constructor(data, left, right) {
+    super()
+    this.data = data
+    this.left = left
+    this.right = right
+  }
+
+  get children() {
+    return [this.left, this.right].filter(v => v !== undefined)
+  }
+
+  find(value) {
+    switch (compare(value, this.data)) {
+      case -1:
+        return this.left.find(value)
+      case 0:
+        return this
+      case 1:
+        return this.right.find(value)
+    }
+  }
+
+  insert(value) {
+    switch (compare(value, this.data)) {
+      case -1:
+        if (this.left === undefined) {
+          this.left = new BinaryTree(value)
+        } else {
+          this.left.insert(value)
+        }
+        break
+      case 1:
+        if (this.right === undefined) {
+          this.right = new BinaryTree(value)
+        } else {
+          this.right.insert(value)
+        }
+    }
+  }
+
+}
+
+const nodeCount = 30
+const randomNumber = function() { return Math.floor(Math.random() * 100) }
+var tree = new BinaryTree(randomNumber())
+for (var i = 0; i < nodeCount; i++) {
+  tree.insert(randomNumber())
+}
 
 const container = document.getElementById('container')
-var canvas = document.createElement(
-  'canvas',
-  {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    style: 'width=100%; height=100%;'
-  }
-)
+const canvas = document.createElement('canvas')
 container.appendChild(canvas)
 
 const drawNode = function(ctx, center) {
@@ -79,7 +122,7 @@ const reload = function() {
   const ctx = canvas.getContext('2d')
   canvas.width = window.innerHeight * scale
   canvas.height = window.innerHeight * scale
-  const origin = [ctx.canvas.width/2, 40]
+  const origin = [ctx.canvas.width/2, 20]
   drawTree(ctx, tree, origin, drawNode)
   drawTree(ctx, tree, origin, drawLabel)
 }
